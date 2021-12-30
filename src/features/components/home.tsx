ced7842c665ba168db9e25764d824da2e7843a67
sahar <s.behbahani71@outlook.com>
2021-12-30 10:15:52 +0330

    add btn

--- src/features/components/home.tsx
+++ src/features/components/home.tsx
@@ -1,7 +1,14 @@
+import { Button } from "@material-ui/core";
 import * as React from "react";
 import { ReactElement } from "react";
+import { DKButton } from "src/core/components/button/button";
 
 const Home = (): ReactElement => {
-  return <>my project</>;
+  return (
+    <>
+      <Button variant="contained">Contained</Button>
+      <DKButton type="cancel">submit</DKButton>
+    </>
+  );
 };
 export default Home;
