ff6fbe456e24413ffa8e6ad3cf0a086a01ad788e
sahar <s.behbahani71@outlook.com>
2021-12-30 10:29:27 +0330

    add card

--- src/features/components/home.tsx
+++ src/features/components/home.tsx
@@ -2,12 +2,22 @@ import { Button } from "@material-ui/core";
 import * as React from "react";
 import { ReactElement } from "react";
 import { DKButton } from "src/core/components/button/button";
+import { DKCard, DKCardBody, DKCardFooter, DKCardHeader } from "src/core/components/card";
 
 const Home = (): ReactElement => {
   return (
     <>
       <Button variant="contained">Contained</Button>
       <DKButton type="cancel">submit</DKButton>
+      <DKCard>
+        <DKCardHeader hasComplexHeader={true} title="mycard"></DKCardHeader>
+        <DKCardBody>
+          <span>sahar</span>
+        </DKCardBody>
+        <DKCardFooter>
+          <span>footer</span>
+        </DKCardFooter>
+      </DKCard>
     </>
   );
 };
