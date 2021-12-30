cff049de0f3316e9adffad5de339fe02704bca4d
samiraakazemii <samirakazemi0203@gmail.com>
2021-12-30 12:43:02 +0330

    test

--- src/features/components/home.tsx
+++ src/features/components/home.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 import { ReactElement } from "react";
 
 const Home = (): ReactElement => {
-  return <>my project</>;
+  return <>my projects</>;
 };
 export default Home;
