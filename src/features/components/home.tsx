1d5d035eb72373b64e1c2556a53c7b06aac57f34
samiraakazemii <samirakazemi0203@gmail.com>
2021-12-31 14:21:25 +0330

    header&dropdown

--- src/features/components/home.tsx
+++ src/features/components/home.tsx
@@ -1,7 +1,36 @@
+
+
 import * as React from "react";
 import { ReactElement } from "react";
+import { DKCardBody, DKCardFooter } from "src/core/components/card";
+import { DKCard } from "src/core/components/card/card";
+import { DKCardHeader } from "src/core/components/card/card-header";
+import CustomizedMenus from "./dropdown";
+
+
 
 const Home = (): ReactElement => {
-  return <>my projects</>;
+  return <>
+          <DKCard>
+            <DKCardHeader 
+              hasComplexHeader={true} 
+              noBorder={true} className={'p-5 ltr'} 
+            >
+            <h3 className={"card-title"} style={{display: "grid"}}>
+               Files
+            <small>More than 400+ new members </small>
+            </h3>
+            <div className="card-toolbar">
+              <CustomizedMenus></CustomizedMenus>
+            </div>
+          
+            </DKCardHeader>
+            <DKCardBody></DKCardBody>
+            <DKCardFooter></DKCardFooter>
+          </DKCard>
+
+          
+        </>
+        ;
 };
 export default Home;
