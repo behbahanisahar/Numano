f87cb6e060895179b2ddbdeb7a98de43634855ff
sahar <s.behbahani71@outlook.com>
2022-01-20 09:47:21 +0330

    add useEffect example

--- src/features/components/useEffectExample.tsx
+++ src/features/components/useEffectExample.tsx
@@ -2,19 +2,20 @@ import React, { useState, useEffect, ReactElement } from "react";
 
 const Example = (): ReactElement => {
   const [count, setCount] = useState(0);
- // const numbers = [1, 2, 3, 4, 5];
- // const listItems = numbers.map(number => <li key={number}>{number * 2}</li>);
+  const numbers: number[] = [1, 2, 3, 4, 5];
+  const listItems = numbers.map(number => <li key={number}>{number * 2}</li>);
+ 
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
-  });
+  }, [count]);
 
   return (
     <div>
       <p>You clicked {count} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
       <button>Don't do anything</button>
-      {/* <ul>{listItems}</ul> */}
+      <ul>{listItems}</ul>
     </div>
   );
 };
